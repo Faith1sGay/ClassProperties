b72ce1a313aa29b874bebe48348892d22c1d11d0
Faith1sGay <daniel.viola001@mymdc.net>
2021-01-25 12:42:19 -0500

    im stupid

---
+++ Flower.cs
@@ -0,0 +1,21 @@
+﻿public class Flower
+{
+    readonly string name;
+    readonly string color;
+    private readonly int petals;
+    public string Color
+    {
+        get { return color; }
+        set { Color = color; }
+    }
+    public int Petals
+    {
+        get { return petals; }
+        set { Petals = value; }
+    }
+    public string Name
+    {
+        get { return name; }
+        set { Name = name; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassProperties
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Sunflower = new Flower { Name = "Sunni", Petals = 8, Color = "Blue" };
+
+        }
+    }
+}
